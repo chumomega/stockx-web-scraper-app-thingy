8005d39ba6fc8d51c7bebb7f825e078ceee3bf98
Chuma Okoro <chuma.okoro76@gmail.com>
2021-12-05 22:20:53 -0500

    hook up apache cassandra to store data scraped from stockx

--- main.py
+++ main.py
@@ -3,32 +3,49 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-import os
 import time
+import datetime
+import random
 from StockxTrendGetter import StockxTrendGetter
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from cassandra.cluster import Cluster
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cluster = Cluster()
+    session = cluster.connect('main_db')
+    # session.set_keyspace('top_products')
 
     top_trend_getter = StockxTrendGetter()
 
     begin = time.perf_counter()
-    top_sneakers = top_trend_getter.get_top_trending_sneakers()
-    top_streetwear = top_trend_getter.get_top_trending_streetwear()
-    end = time.perf_counter()
+    # session.execute('USE top_products')
+    # print("Date: ", )
+    top_sneakers = session.execute('SELECT date, type, product, id from top_products').all()
+    num_top_sneakers = len(top_sneakers)
+    if num_top_sneakers == 0:
+        print("Scraping web for data")
+        top_sneakers = top_trend_getter.get_top_trending_sneakers()
+        for sneaker in top_sneakers:
+            session.execute(
+                """
+                INSERT INTO top_products (date, type, product, id)
+                VALUES (%s, %s, %s, %s)
+                """,
+                (datetime.datetime.now().strftime('%x'), 'sneaker', str(sneaker), random.randint(0,100000))
+            )
+    else:
+        print("Successfully got data from Cassandra")
 
-    print("all the sneakers: ", top_sneakers)
-    print("all the streetwear: ", top_streetwear)
-    print("time_to_run: ", (end - begin))
+    end = time.perf_counter()
+    print("time_to_run sneaker retrieval: ", (end - begin))
+
+    top_streetwear = top_trend_getter.get_top_trending_streetwear()
+
+    print("1st sneaker: ", top_sneakers[0])
+    print("2nd sneaker: ", top_sneakers[1])
+    print("num sneakers in db: ", num_top_sneakers)
+    # print("all the streetwear: ", top_streetwear)
+    cluster.shutdown()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
