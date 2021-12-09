fc3b92458747156edd2913cd94865b60fe48f9ca
Chuma Okoro <chuma.okoro76@gmail.com>
2021-12-08 21:04:33 -0500

    get top sneaker brand from list of sneakers

--- main.py
+++ main.py
@@ -11,17 +11,46 @@ from cassandra.cluster import Cluster
 
 
 # Press the green button in the gutter to run the script.
+def getMostPopularBrand(products: list):
+    brands = list(map(lambda product: product.split()[0], products))
+    # sub_brands = filter(lambda product: product.split()[1], products)
+
+    brand_counter = getItemCounter(brands)
+    # sub_brand_counter = getItemCounter(sub_brands)
+
+    return getMostPopularItem(brand_counter)
+    # top_sub_brand = getMostPopularItem(sub_brand_counter)
+    # return top_brand
+
+
+# Expecting a dictionary with key = str, value = count
+def getMostPopularItem(item_counter: dict) -> str:
+    return max(item_counter, key=item_counter.get)
+
+
+def getItemCounter(items: list) -> dict:
+    item_counter = {}
+
+    for item in items:
+        if item in item_counter:
+            item_counter[item] += 1
+        else:
+            item_counter[item] = 1
+
+    return item_counter
+
+
 if __name__ == '__main__':
     cluster = Cluster()
     session = cluster.connect('main_db')
-    # session.set_keyspace('top_products')
 
     top_trend_getter = StockxTrendGetter()
 
     begin = time.perf_counter()
-    # session.execute('USE top_products')
-    # print("Date: ", )
-    top_sneakers = session.execute('SELECT date, type, product, id from top_products').all()
+    top_sneakers = list(map(
+        lambda row: row.product,
+        session.execute('SELECT date, type, product, id from top_products').all()
+    ))
     num_top_sneakers = len(top_sneakers)
     if num_top_sneakers == 0:
         print("Scraping web for data")
@@ -32,7 +61,7 @@ if __name__ == '__main__':
                 INSERT INTO top_products (date, type, product, id)
                 VALUES (%s, %s, %s, %s)
                 """,
-                (datetime.datetime.now().strftime('%x'), 'sneaker', str(sneaker), random.randint(0,100000))
+                (datetime.datetime.now().strftime('%x'), 'sneaker', str(sneaker), random.randint(0, 100000))
             )
     else:
         print("Successfully got data from Cassandra")
@@ -42,10 +71,9 @@ if __name__ == '__main__':
 
     top_streetwear = top_trend_getter.get_top_trending_streetwear()
 
-    print("1st sneaker: ", top_sneakers[0])
-    print("2nd sneaker: ", top_sneakers[1])
-    print("num sneakers in db: ", num_top_sneakers)
-    # print("all the streetwear: ", top_streetwear)
+    top_brand = getMostPopularBrand(top_sneakers)
+
+    print("Top Brand: ", top_brand)
     cluster.shutdown()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
